1dcbbe1adf4d13c1cfda69f1441da56eef3fe29f
Kelton <kelton@orth.org>
2021-05-25 17:11:10 -0700

    Wrote second test, checks if n%5 == buzz

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -1,9 +1,13 @@
 import unittest
+from fizzbuzz import fizz_buzz
 
 class TestStringMethods(unittest.TestCase):
 
     def test_fizz(self):
         self.assertEqual(fizz_buzz(3), "Fizz")
+
+    def test_buzz(self):
+        self.assertEqual(fizz_buzz(5), "Buzz")
 
 if __name__ == "__main__":
     unittest.main()
