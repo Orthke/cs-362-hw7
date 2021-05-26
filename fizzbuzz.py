032f5d385b16df7e5d9b41590946ae95c3f9582a
Kelton <kelton@orth.org>
2021-05-25 17:14:39 -0700

    Implemented in fizz_buzz function check if n%3 and n%5 == 0 return fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 def fizz_buzz(n):
-    if n%3 == 0:
+    if n%3 == 0 and n%5 == 0:
+        return "FizzBuzz"
+    elif n%3 == 0:
         return "Fizz"
-    if n%5 == 0:
+    elif n%5 == 0:
         return "Buzz"
