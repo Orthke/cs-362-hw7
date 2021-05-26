e6e11ae8e9531a4f46cedccfe9b18766b7b89523
Kelton <kelton@orth.org>
2021-05-25 17:27:06 -0700

    Implemented code in leap_year to check if year divis by 400 and modified original 2000 test with new requirement

--- test_leap_year.py
+++ test_leap_year.py
@@ -4,7 +4,7 @@ from leap_year import leap_year
 class TestStringMethods(unittest.TestCase):
 
     def test_divis4(self):
-        self.assertEqual(leap_year(2000), False)
+        self.assertEqual(leap_year(2000), True)
 
     def test_divis100(self):
         self.assertEqual(leap_year(2100), False)
