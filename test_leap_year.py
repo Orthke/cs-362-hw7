c75e3412c3197034586bf6ee6132b5b5c47f9dd0
Kelton <kelton@orth.org>
2021-05-25 17:22:41 -0700

    Wrote second test now checks that 2100 is not a leap year

--- test_leap_year.py
+++ test_leap_year.py
@@ -3,8 +3,11 @@ from leap_year import leap_year
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_fizz(self):
-        self.assertEqual(leap_year(2000), True)
+    def test_divis4(self):
+        self.assertEqual(leap_year(2000), False)
+
+    def test_divis100(self):
+        self.assertEqual(leap_year(2100), False)
 
 if __name__ == "__main__":
     unittest.main()
